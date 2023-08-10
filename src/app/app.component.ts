86418b790e670e8f1769810bdc309aed87cd4644
Harun Yahya Öztürk <knkhrn@gmail.com>
2023-08-10 17:08:43 +0300

    Frontend part of my e-commerce application. First commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Product} from "./product.model";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Frontend';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+
+    this.http.get<Product[]>('http://localhost:8080/products/getAllProducts').subscribe(data => {
+      this.products = data;
+    });
+  }
 }
