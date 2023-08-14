3cfc84408528d8d749c99d88ee34c9ad9a0d9de0
Harun Yahya Öztürk <knkhrn@gmail.com>
2023-08-14 11:26:58 +0300

    Added type safe Currency and Type for product

--- src/app/product.model.ts
+++ src/app/product.model.ts
@@ -1,8 +1,28 @@
+export enum Currency {
+  DOLLAR = '$',
+  EURO = '€',
+  TURKISH_LIRA = '₺',
+  POUND_STERLING = '£'
+}
+export enum Type {
+  ELECTRONIC = 'Electronic',
+  GARDENING = 'Gardening',
+  FURNITURE = 'Furniture',
+  FASHION = 'Fashion',
+  HOME = 'Home',
+  BOOKS = 'Books',
+  HEALTH = 'Health',
+  TOYS = 'Toys',
+  GROCERIES = 'Groceries',
+  JEWELRY = 'Jewelry',
+  AUTOMOTIVE = 'Automotive'
+}
 export interface Product {
   id: string;
   name: string;
   description: string;
-  type: string;
+  type: Type;
   price: number;
-  currency: string;
+  currency: Currency;
+  
 }
