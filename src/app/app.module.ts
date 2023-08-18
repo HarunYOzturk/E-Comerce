516240b64dfa313098d3fd5317d3d65aaed0b75e
Harun Yahya Öztürk <knkhrn@gmail.com>
2023-08-18 09:39:14 +0300

    visual update, addition of pagination, faulty logic for min-max value by a slider.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgbModule, NgbPaginationModule ,NgbPaginationConfig } from '@ng-bootstr
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +12,11 @@ import { AddProductComponent } from './add-product/add-product.component';
 import { UpdateProductComponent } from './update-product/update-product.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ButtonModule } from "primeng/button";
+import {MultiSelectModule} from "primeng/multiselect";
+import {TableModule} from "primeng/table";
+import {SliderModule} from "primeng/slider";
+
 //
 // const appRoutes: Routes = [
 //   { path: 'product/new', component: AddProductComponent },
@@ -29,12 +34,17 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     NgbModule,
     NgbPaginationModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    MultiSelectModule,
+    TableModule,
+    SliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,10 +12,15 @@ import { debounceTime, switchMap } from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
   products: Product[] = [];
+  productsBeforeRanged: Product[] = [];
   searchTerm: string = '';
   validTypes: string[] = Object.values(Type);
   selectedTypes: Type[] = [];
   private searchSubject = new Subject<string>();
+  sortOrder: 'lowToHigh' | 'highToLow' = 'lowToHigh';
+  minPrice: number = 0;
+  maxPrice: number = 10000;
+  priceRange: number[] = [0, 10000];
 
   constructor(
     private http: HttpClient,
@@ -30,20 +35,21 @@ export class HomeComponent implements OnInit {
       )
       .subscribe(
         (filteredProducts: Product[]) => {
-          this.products = filteredProducts;
+          this.productsBeforeRanged = this.sortProducts(filteredProducts);
+          this.applyFilterByPriceRange();
         },
         (error: any) => {
           console.error('Error fetching filtered products:', error);
         }
       );
 
-    this.search('');
+    this.fetchProducts();
   }
-
 
   fetchProducts(): void {
     this.http.get<Product[]>('http://localhost:8080/products/getAllProducts').subscribe((data) => {
-      this.products = data;
+      this.productsBeforeRanged = this.sortProducts(data);
+      this.applyFilterByPriceRange();
     });
   }
 
@@ -55,17 +61,17 @@ export class HomeComponent implements OnInit {
     const selectedTypeParam = this.selectedTypes.join(',');
 
     if (term.trim() === '') {
-      if (this.selectedTypes.length === 0) {
+      if (this.selectedTypes.length === 0 || this.selectedTypes.sort()[0].toString() === 'All') {
         return this.http.get<Product[]>('http://localhost:8080/products/getAllProducts');
       } else {
         return this.http.get<Product[]>(`http://localhost:8080/products/filterProductsByTypes/${selectedTypeParam}`);
       }
     } else {
-      if (this.selectedTypes.length === 0) {
+      if (this.selectedTypes.length === 0 || this.selectedTypes.sort()[0].toString() === 'All') {
         return this.http.get<Product[]>(`http://localhost:8080/products/search/${term}`);
       } else {
         return this.http.get<Product[]>(
-          `http://localhost:8080/products/filterProducts?term=${term}&type=${selectedTypeParam}`
+          `http://localhost:8080/products/filterProducts?term=${term}&types=${selectedTypeParam}`
         );
       }
     }
@@ -80,11 +86,6 @@ export class HomeComponent implements OnInit {
     } else {
       this.selectedTypes.push(type);
     }
-    this.applyFilter();
-  }
-
-  onSearch(): void {
-    this.search(this.searchTerm);
   }
 
   applyFilterByType(type: Type): void {
@@ -95,17 +96,50 @@ export class HomeComponent implements OnInit {
     } else {
       this.selectedTypes.push(type);
     }
+  }
 
-    this.applyFilter();
+  sortByLowToHigh(): void {
+    this.sortOrder = 'lowToHigh';
+    this.productsBeforeRanged = this.sortProducts(this.productsBeforeRanged);
+    this.applyFilterByPriceRange();
+  }
+
+  sortByHighToLow(): void {
+    this.sortOrder = 'highToLow';
+    this.productsBeforeRanged = this.sortProducts(this.productsBeforeRanged);
+    this.applyFilterByPriceRange();
+  }
+
+  sortProducts(products: Product[]): Product[] {
+    if (this.sortOrder === 'lowToHigh') {
+      return products.sort((a, b) => a.price - b.price);
+    } else if (this.sortOrder === 'highToLow') {
+      return products.sort((a, b) => b.price - a.price);
+    }
+    return products;
+  }
+
+  filterProductsByPriceRange(products: Product[]): Product[] {
+    if (this.minPrice !== null && this.maxPrice !== null) {
+      return products.filter(product => product.price >= this.minPrice && product.price <= this.maxPrice);
+    }
+    return products;
   }
 
 
+  applyFilterByPriceRange(): void {
+    this.products = this.sortProducts(this.filterProductsByPriceRange(this.productsBeforeRanged));
+  }
 
+  onPriceRangeChange(): void {
+    this.applyFilterByPriceRange();
+  }
   onDeleteProduct(productId: string): void {
     const confirmDelete = confirm('Are you sure you want to delete this product?');
     if (confirmDelete) {
       this.http.delete<Product>(`http://localhost:8080/products/deleteProduct/${productId}`).subscribe(() => {
-        this.products = this.products.filter(product => product.id !== productId);
+        this.productsBeforeRanged = this.productsBeforeRanged.filter(product => product.id !== productId);
+        this.applyFilterByPriceRange();
       });
     }
   }
